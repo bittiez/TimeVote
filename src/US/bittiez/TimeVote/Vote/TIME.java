0b86960f50ca5da2c16c45ea99bb62e6497f806d
Tad Taylor <bittiez@gmail.com>
2017-04-09 18:44:47 -0600

    Added clear weather to TIME class

---
+++ src/US/bittiez/TimeVote/Vote/TIME.java
@@ -0,0 +1,17 @@
+package US.bittiez.TimeVote.Vote;
+
+public class TIME {
+    public static final int DAY = 0;
+    public static final int NIGHT = 1;
+    public static final int CLEAR_WEATHER = 2;
+
+    public static String timeString(int time) {
+        if(time == TIME.DAY)
+            return "day";
+        if(time == TIME.NIGHT)
+            return "night";
+        if(time == TIME.CLEAR_WEATHER)
+            return "clear weather";
+        return "";
+    }
+}
